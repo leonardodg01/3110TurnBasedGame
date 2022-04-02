bc3ba9f8c718d3d96a7d28160d1efba1e9d2a20b
Leo <leodgrim117@gmail.com>
2022-04-02 17:31:04 -0500

    Update

---
+++ Assets/Scripts/ScriptableObjects/AbilitiesList.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Enemy", menuName = "Enemy")]
+public class AbilitiesList : ScriptableObject
+{
+    public string type;
+    public int value;
+}
